33a85b6ef25fbebe390eb930974e8c0b07c18a0d
Sergei Legkodymov <rutven@gmail.com>
2023-09-28 22:52:25 +0300

    init commit

---
+++ src/main/java/name/legkodymov/test/springcompose/SpringComposeApplication.java
@@ -0,0 +1,13 @@
+package name.legkodymov.test.springcompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringComposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/name/legkodymov/test/springcompose/SpringComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package name.legkodymov.test.springcompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
